012b5449c69d033af1495c5873877d05902787f8
Johnny <ENDARAJC@innovasi.com>
2023-02-09 16:01:02 -0800

    test31

---
+++ cypress/integration/Testcases/AddSnivel8.js
@@ -0,0 +1,71 @@
+/// <reference types="cypress" />
+
+import { slowCypressDown } from 'cypress-slow-down'
+// slowCypressDown(2000)
+
+import LoginPage from '../pageObjects/LoginPage'
+
+
+import navigationBar  from '../../support/page_objects/NavigationBar'
+import ResourcesPage from '../../support/page_objects/ResourcesPage'
+import { timesSeries } from 'async'
+
+const resourcesPage = new ResourcesPage()
+const navBar = new navigationBar()
+const peopleList = resourcesPage.peopleList
+
+
+
+describe('Login', function()
+{
+  
+  beforeEach(function(){
+    cy.fixture('DATA').then((data) => {
+        cy.visit("/")
+       // cy.visit(Cypress.env('host'))
+       
+       this.data = data
+        cy.login1(this.data.name)
+  
+    })  
+  })
+
+  afterEach(function () {
+
+    navBar.navigateToSignOutMenu() //
+
+  })
+
+it('Add Snivel', function(){  
+
+
+  
+navBar.navigateToResourcesPage().click()
+
+//debugger
+//cy.pause()
+resourcesPage.newBttn().click()
+
+resourcesPage.snivelSel().click()
+
+resourcesPage.peopleSel().click()
+  .type("A").get(peopleList)
+  .contains(peopleList, this.data.peopleListItemName).click()
+ 
+ // resourcesPage.peopleSel().type('{enter}').type('{enter}')
+//resourcesPage.peopleSel().click()
+  //.type('{enter}')
+  
+resourcesPage.assignBttn().click()
+  //cy.contains('Save').click()
+  cy.contains('Cancel').click()
+ 
+
+  })
+
+  
+})
+
+
+
+  
