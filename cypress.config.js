ad51085e8662dddbd12c02b6dc37b09f8c48e096
Johnny <ENDARAJC@innovasi.com>
2023-02-14 02:08:54 -0800

    test9

--- cypress.config.js
+++ cypress.config.js
@@ -1,37 +1,9 @@
-
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  retries:{
-    runMode: 1
+  e2e: {
+    baseUrl: 'https://tsharpslot1qa.devsd.innovasi.com/',
   },
-  pageLoadTimeout: 200000,
-  defaultCommandTimeout: 20000,
   projectId: '1huejn',
-
-  e2e: {setupNodeEvents(on, config) {
-
-      
-    }, 
-    
-    baseUrl:'https://tsharpslot1qa.devsd.innovasi.com/',
-    //baseUrl:'https://sharptnrslot0qa.devsd.innovasi.com/',
-    specPattern:'cypress/integration/Testcases/*.js'
-    //specPattern:'cypress/e2e/1-getting-started/*.cy.js'
-    //specPattern:'cypress/e2e/2-advanced-examples/*.cy.js'
-  },
-  watchForFileChanges:true,
-  reporter: "mochawesome",
-  reporterOptions:{
-    charts:true,
-    overwrite:false,
-    html:false,
-    json:true,
-    reportDir:"cypress/reports"
-
-  }
- 
-
-
+  specPattern:'cypress/integration/Testcases/*.js'
 })
-
