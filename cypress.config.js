d706c82dde4d01bb8f36b3f24000acb948d51285
Johnny <ENDARAJC@innovasi.com>
2023-02-18 22:08:49 -0800

    cypress demo 14

--- cypress.config.js
+++ cypress.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
     });
     
     }, 
-    baseUrl:'https://tsharpslot1qa.devsd.innovasi.com/',
+    //baseUrl:'https://tsharpslot1qa.devsd.innovasi.com/',
     // baseUrl:'https://sharpproductionmirror.devsd.innovasi.com/',
      //baseUrl:'https://www.google.com',
     //specPattern:'cypress/integration/Testcases/*.js',
